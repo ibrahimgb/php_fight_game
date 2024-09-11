a77ffad9bd95a874b654dfb8c0ee293b8fbfbfe1
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-09-11 12:59:40 +0200

    add display

--- src/Fight.php
+++ src/Fight.php
@@ -1,51 +1,33 @@
 <?php
 
 require_once 'Player.php';
+require_once 'FightDisplay.php';
 
 class Fight {
     private $playerA;
     private $playerB;
+    private $display;
 
     public function __construct(Player $playerA, Player $playerB) {
         $this->playerA = $playerA;
         $this->playerB = $playerB;
+        $this->display = new FightDisplay($this->playerA, $this->playerB);
     }
 
     public function run($turn = 1) {
         if ($this->playerA->isAlive() && $this->playerB->isAlive()) {
             if ($turn == 1) {
                 $this->playerB->takeDamage($this->playerA->attack);
-                $this->printScore();
+                $this->display->printScore();
                 $this->run(0);
             } else {
                 $this->playerA->takeDamage($this->playerB->attack);
-                $this->printScore();
+                $this->display->printScore();
                 $this->run(1);
             }
         } else {
-            $this->printFinalResult();
+            $this->display->printFinalResult();
         }
-    }
-
-    private function printFinalResult() {
-        $playerA_alive = $this->playerA->isAlive();
-        $playerB_alive = $this->playerB->isAlive();
-
-        switch (true) {
-            case $playerA_alive:
-                echo "<br>" . $this->playerA->name . " wins with " . $this->playerA->hp . " HP left.<br>";
-                break;
-            case $playerB_alive:
-                echo "<br>" . $this->playerB->name . " wins with " . $this->playerB->hp . " HP left.<br>";
-                break;
-            default:
-                echo "It's a draw! Neither player is alive.<br>";
-                break;
-        }
-    }
-
-    private function printScore() {
-        echo "<br>" . $this->playerA->hp . "        " . $this->playerB->hp . " HP<br>";
     }
 }
 

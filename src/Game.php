4b4a838f8274d90c3cf86ff483e9f0fbcc072076
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-09-11 12:54:12 +0200

    split game class

--- src/Game.php
+++ src/Game.php
@@ -6,7 +6,6 @@ require_once 'Fight.php';
 class Game {
     private $playerA;
     private $playerB;
-    private $combat;
 
     public function __construct($input) {
         $lines = explode("\n", trim($input));
@@ -16,14 +15,14 @@ class Game {
 
         $playerB_info = explode(" ", trim($lines[1]));
         $this->playerB = new Player($playerB_info[0], $playerB_info[1], $playerB_info[2], $playerB_info[3]);
-
-        $this->combat = new Fight($this->playerA, $this->playerB);
     }
 
     public function startGame() {
         echo "<br>" . $this->playerA->name . " - HP: " . $this->playerA->hp . ", Attack: " . $this->playerA->attack . ", Defense: " . $this->playerA->defense . "<br>";
         echo "<br>" . $this->playerB->name . " - HP: " . $this->playerB->hp . ", Attack: " . $this->playerB->attack . ", Defense: " . $this->playerB->defense . "<br>";
-        $this->combat->run(1);
+
+        $fight = new Fight($this->playerA, $this->playerB);
+        $fight->run(1);
     }
 }
 

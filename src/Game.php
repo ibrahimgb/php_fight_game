66de1d31ebc083c66aa55f9c124c3163ae0831a6
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-09-11 02:40:35 +0200

    correct output return to line

--- src/Game.php
+++ src/Game.php
@@ -14,7 +14,9 @@ class Player {
     }
 
     public function takeDamage($damage) {
-        $this->hp -= $damage;
+        $damageTaken = $damage - $this->defense;
+        if ($damageTaken < 0) $damageTaken = 0;
+        $this->hp -= $damageTaken;
         if ($this->hp < 0) {
             $this->hp = 0; 
         }
@@ -29,8 +31,6 @@ class Game {
     private $playerA;
     private $playerB;
 
-    private $players =  [$this->playerA, $this->playerB ];
-
     public function __construct($input) {
         $lines = explode("\n", trim($input));
 
@@ -41,40 +41,52 @@ class Game {
         $this->playerB = new Player($playerB_info[0], $playerB_info[1], $playerB_info[2], $playerB_info[3]);
     }
 
-
-    private function run( $x = 1) {
-
-        if($x==1){
-            $this->players[0]->takeDamage($this->players[1]->attack);
-            $this->print_score();
-            $this->run(0);
+    public function run($turn = 1) {
+        if ($this->playerA->isAlive() && $this->playerB->isAlive()) {
+            if ($turn == 1) {
+                $this->playerB->takeDamage($this->playerA->attack);
+                $this->print_score();
+                $this->run(0);
+            } else {
+                $this->playerA->takeDamage($this->playerB->attack);
+                $this->print_score();
+                $this->run(1);
+            }
+        } else {
+            $this->print_final_result();
         }
-        if($x==1){
-            $this->players[1]->takeDamage($this->players[0]->attack);
-            $this->print_score();
-            $this->run(1);
-        }
-
-        if($this->playerA->isAlive() ||  $this->playerB->isAlive()){
-            
-            return;
-        } 
     }
 
+    private function print_final_result() {
+        $playerA_alive = $this->playerA->isAlive();
+        $playerB_alive = $this->playerB->isAlive();
 
-    private function print_score(){
-        echo($this->playerA->hp + " " * $this->playerB->hp );
-        echo($this->playerA->hp + " " * $this->playerB->hp );
+
+        switch (true) {
+            case $playerA_alive:
+
+                echo "<br>" . $this->playerA->name . " wins with " . $this->playerA->hp . " HP left.\n" . "<br>" ;
+                break;
+            case $playerB_alive:
+                echo "<br>" . $this->playerB->name . " wins with " . $this->playerB->hp . " HP left.\n". "<br>" ;
+                break;
+            default:
+                echo "It's a draw! Neither player is alive.\n";
+                break;
+        }
     }
 
-    public function start_game(){
-        echo($this->playerA->name + " " * $this->playerA->hp + " " + $this->playerA->attack + " " + $this->playerA->defense );
-        echo($this->playerB->name + " " * $this->playerB->hp + " " + $this->playerB->attack + " " + $this->playerB->defense );
-        $this->run(0);
+    private function print_score() {
+        echo "<br>" . $this->playerA->hp . "        " . $this->playerB->hp . " HP\n" . "<br>" ;
+    }
+
+    public function start_game() {
+        echo "<br>" . $this->playerA->name . " - HP: " . $this->playerA->hp . ", Attack: " . $this->playerA->attack . ", Defense: " . $this->playerA->defense . "\n" , "<br>";
+        echo "<br>" . $this->playerB->name . " - HP: " . $this->playerB->hp . ", Attack: " . $this->playerB->attack . ", Defense: " . $this->playerB->defense . "\n" , "<br>";
+        $this->run(1);
     }
 }
 
-// Example usage
 $data = "Bob 30 7 4\nAlice 20 9 2";
 $game = new Game($data);
 $game->start_game();
